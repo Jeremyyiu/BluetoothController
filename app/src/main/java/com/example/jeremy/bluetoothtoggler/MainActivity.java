7609a7b3e7debce586a8c5bb67b6bac7ce304a10
Jeremy Yiu <utsjeremy@gmail.com>
2018-03-06 03:13:20 +1100

    Added bluetooth listener - changes the slider accordingly from bluetooth changes outside the app.

--- app/src/main/java/com/example/jeremy/bluetoothtoggler/MainActivity.java
+++ app/src/main/java/com/example/jeremy/bluetoothtoggler/MainActivity.java
@@ -3,19 +3,23 @@ package com.example.jeremy.bluetoothtoggler;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.database.ContentObserver;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
 import android.provider.Settings;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.Toast;
 
 import com.gc.materialdesign.views.Switch;
 
-
 public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter btAdapter = null;
+    BluetoothObserver bluetoothObserver = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,11 +29,16 @@ public class MainActivity extends AppCompatActivity {
 
         Switch bluetoothSwitch = (Switch) findViewById(R.id.bluetoothSwitch);
         initSwitch(bluetoothSwitch);
+
+        final Uri BLUETOOTH_URL = Settings.System.getUriFor(Settings.System.BLUETOOTH_ON);
+        bluetoothObserver = new BluetoothObserver(new Handler());
+        getApplicationContext().getContentResolver()
+                .registerContentObserver(BLUETOOTH_URL, true, bluetoothObserver);
     }
 
     private void initSwitch(Switch bluetoothSwitch) {
         int bluetoothStatus = checkBluetoothStatus();
-        if(bluetoothStatus == 1) {
+        if (bluetoothStatus == 1) {
             bluetoothSwitch.setChecked(true);
         } else {
             bluetoothSwitch.setChecked(false);
@@ -43,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
                 //Checks if this app can modify system settings
                 boolean canWriteSettings = Settings.System.canWrite(context);
-                if(canWriteSettings) {
-                    if(check) {
+                if (canWriteSettings) {
+                    if (check) {
                         turnOnBluetooth();
                     } else {
                         turnOffBluetooth();
@@ -66,7 +75,6 @@ public class MainActivity extends AppCompatActivity {
         btAdapter.disable();
     }
 
-
     /**
      * Shows the modify system settings panel to allow the user to add WRITE_SETTINGS permissions for this app.
      */
@@ -77,13 +85,41 @@ public class MainActivity extends AppCompatActivity {
 
     public int checkBluetoothStatus() {
         int status = btAdapter.getState();
-        if(status == btAdapter.STATE_ON) {
+        if (status == btAdapter.STATE_ON) {
             return 1;
-        }
-        else if(status == btAdapter.STATE_OFF) {
+        } else if (status == btAdapter.STATE_OFF) {
             return 0;
         }
         //returns -1, as it is either STATE_TURNING_ON or STATE_TURNING_OFF
         return -1;
+    }
+
+    private void bluetoothToggle() {
+        Switch bluetoothSwitch = findViewById(R.id.bluetoothSwitch);
+        if (bluetoothSwitch.isCheck()) {
+            bluetoothSwitch.setChecked(false);
+        } else {
+            bluetoothSwitch.setChecked(true);
+        }
+    }
+
+    /**
+     * Bluetooth Observer: Handle the change in bluetooth connection in real time and change whether the switch is checked or not
+     */
+    private class BluetoothObserver extends ContentObserver {
+        public BluetoothObserver(Handler h) {
+            super(h);
+        }
+
+        @Override
+        public boolean deliverSelfNotifications() {
+            return true;
+        }
+
+        @Override
+        public void onChange(boolean selfChange) {
+            super.onChange(selfChange);
+            bluetoothToggle();
+        }
     }
 }
